c79bdab7fcb34d84097c66ae7d8a1e0bd8f50340
NongEy <phanthakan@sirisoft.co.th>
2024-02-05 11:54:45 +0700

    test

--- go-coverage-test.sh
+++ go-coverage-test.sh
@@ -73,6 +73,7 @@ if [[ -n $EXCLUDE ]]; then
     PKGS=$(echo "$PKGS" | grep -v -E "${EXCLUDE}")
 fi
 COVERAGE_FILE="${BUILD_DIR}/coverage.out"
+echo "${BUILD_DIR}"
 # Create build dir
 mkdir -p "${BUILD_DIR}"
 # Step 1: Run tests with coverage
@@ -81,7 +82,7 @@ go test $PKGS -coverprofile="${COVERAGE_FILE}"
 for p in "${EXCLUDE_FILES[@]}"; do
     sed -i.bak "/${p//\//\\/}/d" "${COVERAGE_FILE}"
 done
-# Step 3: Check coverage and compare with the tdhreshold
+# Step 3: Check coverage and compare with the threshold
 COVERAGE=$(go tool cover -func="${COVERAGE_FILE}" | grep total | awk '{print substr($3, 1, length($3)-1)}')
 echo "Total coverage: ${COVERAGE}%. Threshold: ${THRESHOLD}%."
 if (($(echo "${COVERAGE} < ${THRESHOLD}" | bc -l))); then
